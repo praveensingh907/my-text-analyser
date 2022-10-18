83794ab7585e0b0f967c38c3f6f1755855cf1253
praveensingh907 <singh.kr.praveen.907@gmail.com>
2022-10-18 16:16:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 import Navbar from './compnents/Navbar';
 import Text from './compnents/TextForm';
-// import About from './compnents/About';
+import Alert from './compnents/Alert';
+import About from './compnents/About';
+
 
 
 
 function App() {
+
+  const [Mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    });
+
+    setTimeout(() => {
+      setAlert(null);
+    },1500);
+  }
+
+
+  const toggleMode = ()=>{
+    if(Mode==="dark"){
+      setMode("light");
+      document.body.style.backgroundColor="white";
+      showAlert("light mode has been Enabled","success");
+      document.title='TextUtils - light mode';
+
+      // flashing title 
+      // setInterval(() => {
+      //   document.title='TextUtils is amazing';
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title='Download TextUtils now';
+      // }, 1500);
+    }else{
+      setMode("dark");
+      document.body.style.backgroundColor="#042743";
+      showAlert("dark mode has been Enabled","success");
+      document.title='TextUtils - dark mode';
+    }
+  }
+
   return (
     <>
-     <Navbar title="Textutils" aboutText="About Us"/>
+     <Navbar title="Textutils" aboutText="About us" mode={Mode} toggleMode={toggleMode}/>
+     <Alert Alert={alert}/>
      <div className="container my-3">
-      <Text heading="Enter the text to analyze below"/>
+   
+      {/* /users---->component1
+      /users/home---->component2
+      but if you will not write exact everytime it will open component1
+      so its important to write exact */}
+        
+         
+          <Text alert={showAlert} heading="Enter the text to analyze below" mode={Mode}/>
+      
+      
       {/* <About/>  */}
+       
      </div>
      
     </>
--- src/compnents/TextForm.js
+++ src/compnents/TextForm.js
@@ -6,20 +6,23 @@ export default function TextForm(props) {
     const [Text,setText]= useState("");
 
 
-
+    
   
 
     const handleUpClick = ()=>{
         let newText = Text.toUpperCase();
         setText(newText);
+        props.alert("all the text changed to uppercase","success");
     }
     const handleLoClick = ()=>{
         let newText = Text.toLocaleLowerCase();
         setText(newText);
+        props.alert("all the text changed to lowercase","success");
     }
     const handleClear = ()=>{
         let newText = "";
         setText(newText);
+        props.alert("all the text has been cleared","success");
     }
     const handleOnChange = (event)=>{
         setText(event.target.value);
@@ -30,19 +33,21 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        props.alert("text copied to clipboard","success");
     }
 
     const handleExtraSpace = ()=>{
         let newText = Text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.alert("Extra spaces removed","success");
     }
     
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color:props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" value={Text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            <textarea className="form-control"  value={Text} onChange={handleOnChange} style={{background:props.mode==='light'?'white':'grey',color:props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary my-2 mx-2" onClick={handleUpClick}>convert to uppercase</button>
         <button className="btn btn-primary my-2 mx-2" onClick={handleLoClick} >convert to lowercase</button>
@@ -56,7 +61,7 @@ export default function TextForm(props) {
         <p>{Text.split(" ").length} words, {Text.length} characters</p>
         <p>{0.008*Text.split(" ").length} minutes to read</p>
         <h3>Preview</h3>
-        <p>{Text}</p>
+        <p>{Text.length>0?Text:"Enter somthing to preview here"}</p>
         {/* <h3>Email present</h3>
         <p>{email}</p> */}
     </div>
